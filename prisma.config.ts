a5890950555e25818ed1343825fb7fb366c97f67
ujangdoubleday <domba88galing@gmail.com>
2025-12-05 14:40:18 +0700

    feat(prisma): add initial prisma schema and migration

---
+++ prisma.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'prisma/config';
+import { config } from 'dotenv';
+
+config({ path: '.env' });
+
+export default defineConfig({
+  schema: './prisma/schema.prisma',
+  datasource: {
+    url: process.env.DATABASE_URL || '',
+  },
+});
